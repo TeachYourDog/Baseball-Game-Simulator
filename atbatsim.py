397479c7ae237338ea731039b570e718c3e20821
Matt Breen <mcjbreen@gmail.com>
2020-08-29 02:39:47 -0500

    Add files via upload
    Eliminated nested loops, defined two functions (atbat, simulate)
    Utilizing Tom Tango's Odds Ratio Method to determine whether or not strikeout or BIP occurs.

--- atbatsim.py
+++ atbatsim.py
@@ -1,26 +1,30 @@
-from random import randint
+import random
 
-trials=100000
-
-
-kprob=1
-walkprob=2
-hitprob=3
-
-strikeouts=0
-walks=0
-hits=0
+hitodds=.300
+pitchodds=.200
+leagueodds=.400
 
 
-for trial in range(trials):
-    pitch=randint(1,3)
-    if pitch == kprob:
-        strikeouts += 1
-    elif pitch == walkprob:
-        walks += 1
-    elif pitch == hitprob:
-        hits +=1
 
-print(strikeouts)
-print(walks)
-print(hits)
+def atbat():
+    abodds=hitodds*(pitchodds/leagueodds)
+    if random.random() <= abodds:
+        return 0
+    else:
+        return 1
+
+def simulate(n):
+    strikeout=[]
+    bip=[]
+    for i in range(n):
+        if atbat() == 0:
+            strikeout.append(atbat())
+        elif atbat() == 1:
+            bip.append(atbat())
+    return (sum(strikeout)/n), (sum(bip)/n)
+
+
+x,y=simulate(10000)
+
+print(x)
+print(y)
