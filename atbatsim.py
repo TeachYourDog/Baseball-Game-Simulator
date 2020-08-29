454a3e4aadcc2fec3a592b9f1c5b08136a28f94b
Matt Breen <mcjbreen@gmail.com>
2020-08-29 03:04:26 -0500

    Add files via upload

--- atbatsim.py
+++ atbatsim.py
@@ -1,30 +1,29 @@
 import random
 
-hitodds=.300
-pitchodds=.200
-leagueodds=.400
+
+#Hitter, Pitcher and League probability of strikeouts
+hitodds=.230
+pitchodds=.40
+leagueodds=.50
 
 
-
+#Determines whether or not a strikeout occurs
 def atbat():
+    ks=0
     abodds=hitodds*(pitchodds/leagueodds)
-    if random.random() <= abodds:
-        return 0
-    else:
-        return 1
+    for i in range(1000):
+        if random.random() <= abodds:
+            ks += 1
+        return ks
 
+
+#simulates at bats n number of times, returning probability of strikeout
 def simulate(n):
-    strikeout=[]
-    bip=[]
+    trials=[]
     for i in range(n):
-        if atbat() == 0:
-            strikeout.append(atbat())
-        elif atbat() == 1:
-            bip.append(atbat())
-    return (sum(strikeout)/n), (sum(bip)/n)
+        trials.append(atbat())
+    return(1-(sum(trials)/n))
 
 
-x,y=simulate(10000)
 
-print(x)
-print(y)
+print(simulate(10000))
