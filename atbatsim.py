aceb4050faf7458496b2fe5301646de99115d851
Matt Breen <mcjbreen@gmail.com>
2020-08-30 20:40:19 -0500

    Add files via upload

--- atbatsim.py
+++ atbatsim.py
@@ -1,29 +1,77 @@
 import random
 
-
 #Hitter, Pitcher and League probability of strikeouts
-hitodds=.230
-pitchodds=.60
-leagueodds=.50
+hkodds=.210
+pkodds=.24
+lkodds=.22
+
+hbbodds=.210
+pbbodds=.24
+lbbodds=.22
+
+hhitodds=.210
+phitodds=.24
+lhitodds=.22
+
 
 
 #Determines whether or not a strikeout occurs
-def atbat():
+def strikeout():
     ks=0
-    abodds=hitodds*(pitchodds/leagueodds)
+    kodds=hkodds*(pkodds/lkodds)
     for i in range(1000):
-        if random.random() <= abodds:
+        if random.random() <= kodds:
             ks += 1
         return ks
 
 
 #simulates at bats n number of times, returning probability of strikeout
-def simulate(n):
+def ksim(n):
     trials=[]
     for i in range(n):
-        trials.append(atbat())
+        trials.append(strikeout())
     return(sum(trials)/n)
 
 
 
-print(simulate(10000))
+#Determines whether or not a walk occurs
+def walk():
+    bbs=0
+    bbodds=hbbodds*(pbbodds/lbbodds)
+    for i in range(1000):
+        if random.random() <= bbodds:
+            bbs += 1
+        return bbs
+
+
+#simulates at bats n number of times, returning probability of walk
+def bbsim(n):
+    trials=[]
+    for i in range(n):
+        trials.append(walk())
+    return(sum(trials)/n)
+
+
+#Determines whether or not a hits occurs
+def hit():
+    hits=0
+    hitodds=hhitodds*(phitodds/lhitodds)
+    for i in range(1000):
+        if random.random() <= hitodds:
+            hits += 1
+        return hits
+
+
+#simulates at bats n number of times, returning probability of hits
+def hitsim(n):
+    trials=[]
+    for i in range(n):
+        trials.append(hit())
+    return(sum(trials)/n)
+
+
+x=ksim(100000)
+y=bbsim(100000)
+z=hitsim(100000)
+
+print(x+y+z)
